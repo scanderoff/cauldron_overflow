b160a07e30e83d29777d3a1cbffd59e599d1fed5
scanderoff <scanderoff@gmail.com>
2022-04-06 17:05:51 +0800

    Filling database

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Service\MarkdownHelper;
+use App\Entity\Question;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class QuestionController extends AbstractController
 {
@@ -29,13 +31,61 @@ class QuestionController extends AbstractController
     }
 
     /**
+     * @Route("/questions/new")
+     */
+    public function new(EntityManagerInterface $entityManager): Response
+    {
+        $question = new Question();
+        $question
+            ->setName('Missing pants')
+            ->setSlug('missing-pants-'.rand(0, 1000))
+            ->setQuestion(
+<<<EOF
+    Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+    to make my dishes wash themselves. But while I was casting it,
+    I slipped a little and I think `I also hit my pants with the spell`.
+
+    When I woke up this morning, I caught a quick glimpse of my pants
+    opening the front door and walking out! I've been out all afternoon
+    (with no pants mind you) searching for them.
+
+    Does anyone have a spell to call your pants back?         
+EOF
+            );
+
+        if (rand(1, 10)) {
+            $question->setAskedAt(new \DateTimeImmutable(sprintf('-%d days', rand(1, 100))));
+        }
+
+        $entityManager->persist($question);
+        $entityManager->flush();
+
+        return new Response(sprintf(
+            'Well hallo! The shiny new question is id #%d, slug %s',
+            $question->getId(),
+            $question->getSlug(),
+        ));
+    }
+
+    /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper)
+    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
         }
+
+        $repository = $entityManager->getRepository(Question::class);
+        /** @var Question/null $question */
+        $question = $repository->findOneBy(['slug' => $slug]);
+
+        if (!$question) {
+            throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
+        }
+
+
+        dd($question);
 
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
