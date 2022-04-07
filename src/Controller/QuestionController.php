1be9af287e8c0b61993e5a88977e993ac8c83386
scanderoff <scanderoff@gmail.com>
2022-04-07 23:31:07 +0800

    Blabla

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -7,6 +7,7 @@ use App\Entity\Question;
 use App\Repository\QuestionRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
@@ -46,21 +47,23 @@ class QuestionController extends AbstractController
             ->setSlug('missing-pants-'.rand(0, 1000))
             ->setQuestion(
 <<<EOF
-    Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
-    to make my dishes wash themselves. But while I was casting it,
-    I slipped a little and I think `I also hit my pants with the spell`.
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
 
-    When I woke up this morning, I caught a quick glimpse of my pants
-    opening the front door and walking out! I've been out all afternoon
-    (with no pants mind you) searching for them.
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
 
-    Does anyone have a spell to call your pants back?         
+Does anyone have a spell to call your pants back?
 EOF
             );
 
         if (rand(1, 10)) {
             $question->setAskedAt(new \DateTimeImmutable(sprintf('-%d days', rand(1, 100))));
         }
+
+        $question->setVotes(rand(-20, 50));
 
         $entityManager->persist($question);
         $entityManager->flush();
@@ -75,18 +78,10 @@ EOF
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
+    public function show(Question $question)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
-        }
-
-        $repository = $entityManager->getRepository(Question::class);
-        /** @var Question/null $question */
-        $question = $repository->findOneBy(['slug' => $slug]);
-
-        if (!$question) {
-            throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
         }
 
         $answers = [
@@ -98,6 +93,25 @@ EOF
         return $this->render('question/show.html.twig', [
             'question' => $question,
             'answers' => $answers,
+        ]);
+    }
+
+    /**
+     * @Route("questions/{slug}/vote", name="app_question_vote", methods="POST")
+     */
+    public function questionVote(Question $question, Request $request, EntityManagerInterface $entityManager)
+    {
+        $direction = $request->request->get('direction');
+
+        if ($direction === 'up')
+            $question->upVote();
+        elseif ($direction === 'down')
+            $question->downVote();
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_question_show', [
+            'slug' => $question->getSlug(),
         ]);
     }
 }
--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -37,6 +37,11 @@ class Question
      */
     private $askedAt;
 
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $votes = 0;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -86,6 +91,39 @@ class Question
     public function setAskedAt(?\DateTimeImmutable $askedAt): self
     {
         $this->askedAt = $askedAt;
+
+        return $this;
+    }
+
+    public function getVotes(): int
+    {
+        return $this->votes;
+    }
+
+    public function getVotesString(): string
+    {
+        $prefix = ($this->getVotes() >= 0) ? '+' : '-';
+        dump($this->getVotes());
+        return sprintf('%s %d', $prefix, abs($this->getVotes()));
+    }
+
+    public function setVotes(int $votes): self
+    {
+        $this->votes = $votes;
+
+        return $this;
+    }
+
+    public function upVote(): self
+    {
+        $this->votes++;
+
+        return $this;
+    }
+
+    public function downVote(): self
+    {
+        $this->votes--;
 
         return $this;
     }
